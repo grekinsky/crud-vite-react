c9a9abb76c77d77905ed5e94461cf3343ce59c79
Greco Rubio <greco.rubio@rubrik.com>
2023-11-28 11:50:08 -0600

    Añadir material-ui global y a lista de cafes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import './App.css';
 import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Link, useParams, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link as RouterLink,
+  useParams,
+  Routes,
+} from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CoffeeIcon from '@mui/icons-material/Coffee';
 import store from './store';
 import CoffeeList from './components/CoffeeList';
 import ModifyCoffeeForm from './components/ModifyCoffeeForm';
 import AddCoffeeForm from './components/AddCoffeeForm';
+import { AppBar, Box, Container, Link, Stack, Toolbar, Typography } from '@mui/material';
 
 const ModifyCoffeeFormScreen = () => {
   const { coffeeId } = useParams<{ coffeeId: string }>();
@@ -13,36 +22,51 @@ const ModifyCoffeeFormScreen = () => {
   return <ModifyCoffeeForm id={parsedcoffeeId} />;
 };
 
+const defaultTheme = createTheme();
+
 const App: React.FC = () => (
-  <Provider store={store}>
+  <ThemeProvider theme={defaultTheme}>
+    <CssBaseline />
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Coffee List</Link>
-            </li>
-            <li>
-              <Link to="/add">Add a Coffee</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/add" element={<AddCoffeeForm />} />
-          <Route path="/modify/:coffeeId" element={<ModifyCoffeeFormScreen />} />
-          <Route
-            path="/"
-            element={
-              <div>
-                <h1>Coffee List</h1>
-                <CoffeeList />
-              </div>
-            }
-          />
-        </Routes>
-      </div>
+      <Provider store={store}>
+        <AppBar component="nav" position="relative">
+          <Container maxWidth="lg">
+            <Toolbar>
+              <CoffeeIcon sx={{ mr: 2 }} />
+              <Typography
+                variant="h6"
+                color="inherit"
+                noWrap
+                component="div"
+                sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+              >
+                Coffee Recipes
+              </Typography>
+              <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                <Stack direction="row" spacing={2}>
+                  <Link component={RouterLink} to="/" sx={{ color: '#fff' }}>
+                    Coffee List
+                  </Link>
+                  <Link component={RouterLink} to="/add" sx={{ color: '#fff' }}>
+                    Add a Coffee
+                  </Link>
+                </Stack>
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+        <main>
+          <Container maxWidth="lg" sx={{ py: 4 }}>
+            <Routes>
+              <Route path="/add" element={<AddCoffeeForm />} />
+              <Route path="/modify/:coffeeId" element={<ModifyCoffeeFormScreen />} />
+              <Route path="/" element={<CoffeeList />} />
+            </Routes>
+          </Container>
+        </main>
+      </Provider>
     </Router>
-  </Provider>
+  </ThemeProvider>
 );
 
 export default App;
--- src/components/CoffeeList.tsx
+++ src/components/CoffeeList.tsx
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../store';
 import { deleteCoffee, fetchCoffees } from '../store/coffeeSlice';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
+import { Button, Card, CardActions, CardContent, CardMedia, Grid, Typography } from '@mui/material';
 
 const CoffeeList: React.FC = () => {
   const { coffees, status } = useSelector((state: RootState) => state.coffees);
@@ -21,23 +22,41 @@ const CoffeeList: React.FC = () => {
   }
 
   return (
-    <ul>
+    <Grid container spacing={4}>
       {coffees.map((coffee) => (
-        <li key={coffee.id}>
-          {coffee.title}{' '}
-          <Link to={`/modify/${coffee.id}`}>
-            <button>Modify</button>
-          </Link>
-          <button
-            onClick={() => {
-              handleRemoveCoffee(coffee.id);
-            }}
-          >
-            X
-          </button>
-        </li>
+        <Grid item key={coffee.id} xs={12} sm={6} md={4}>
+          <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+            <CardMedia
+              component="div"
+              sx={{
+                // 16:9
+                pt: '56.25%',
+              }}
+              image={coffee.image}
+            />
+            <CardContent sx={{ flexGrow: 1 }}>
+              <Typography gutterBottom variant="h5" component="h2">
+                {coffee.title}
+              </Typography>
+              <Typography>{coffee.description}</Typography>
+            </CardContent>
+            <CardActions>
+              <Button component={RouterLink} to={`/modify/${coffee.id}`}>
+                Edit
+              </Button>
+              <Button
+                size="small"
+                onClick={() => {
+                  handleRemoveCoffee(coffee.id);
+                }}
+              >
+                Delete
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
       ))}
-    </ul>
+    </Grid>
   );
 };
 
